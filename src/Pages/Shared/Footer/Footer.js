c530eeae071b459c81cf93dd1983dfd977805590
Md-Tur <turibnmostafiz@gmail.com>
2022-05-08 00:09:47 +0600

    footer design added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            <small>@Footer</small>
-        </div>
+        <footer className='mt-3 text-white bg-dark text-center p-3'>
+            <p><small>Copyright Mega Grocery &copy; {year}</small></p>
+        </footer>
     );
 };
 
