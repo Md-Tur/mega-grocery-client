f0592b1d9e509b49b1886bf40e0168be9b59ee1e
Md-Tur <turibnmostafiz@gmail.com>
2022-05-08 00:39:36 +0600

    all products shown in UI

---
+++ src/Pages/Items/AllItems/AllItems.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const AllItems = ({ product }) => {
+
+    const { name, img, description, price, quantity, supplier } = product;
+
+    return (
+        <div style={{ position: 'relative' }} className='col col-md-1 col-lg-4'>
+            <div>
+                <div className='d-flex justify-content-center'>
+                    <img style={{ height: "200px", width: "350px" }} src={img} alt="" />
+                </div>
+                <div>
+                    <h2> {name} </h2>
+                    <p><small> {description} </small></p>
+                    <p>Price: {price} </p>
+                    <p>Quantity: {quantity} </p>
+                    <p>Supplier: {supplier} </p>
+                </div>
+                <div style={{ bottom: '0' }} className='d-flex justify-content-center'>
+                    <Link to='/'>
+                        <Button variant="outline-primary">Delete</Button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AllItems;
--- src/Pages/Items/ManageItems/ManageItems.js
+++ src/Pages/Items/ManageItems/ManageItems.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import useProducts from '../../../hooks/useProducts';
+import AllItems from '../AllItems/AllItems';
 
 const ManageItems = () => {
+
+    const [products] = useProducts();
+
     return (
         <div>
-            <h2>Manage items</h2>
+            <h1 className='text-center text-primary mt-2 mb-2'>Our All Products</h1>
+
+            <div className='container'>
+                <div className='row g-4'>
+                    {
+                        products.map(product => <AllItems
+                            key={product.id}
+                            product={product}
+                        ></AllItems>)
+                    }
+                </div>
+            </div>
+
         </div>
     );
 };
