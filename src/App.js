308d17ad71155bee75e8b5e3ea2d4d4f36091ea9
Md-Tur <turibnmostafiz@gmail.com>
2022-05-07 22:03:28 +0600

    items route added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Blogs from './Pages/Blogs/Blogs';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import ManageItems from './Pages/Items/ManageItems/ManageItems';
+import AddItems from './Pages/Items/AddItems/AddItems';
+import MyItems from './Pages/Items/MyItems/MyItems';
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/manageitems' element={<ManageItems></ManageItems>}></Route>
+        <Route path='/additems' element={<AddItems></AddItems>}></Route>
+        <Route path='/myitems' element={<MyItems></MyItems>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
