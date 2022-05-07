7b21ff88fe0df03bda61a600be7ede50c1090ad7
Md-Tur <turibnmostafiz@gmail.com>
2022-05-07 23:41:33 +0600

    conditional rendering for login and sign out

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,34 +1,54 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo from '../../../images/logo.png';
 
 const Header = () => {
+
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
     return (
-        <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    <span>Mega Grocery</span>
-                    <img height={45} src={logo} alt="" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/manageitems">Manage Items</Nav.Link>
-                        <Nav.Link as={Link} to="/additems">Add Items</Nav.Link>
-                        <Nav.Link as={Link} to="/myitems">My Items</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/login">
-                            Login
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <span>Mega Grocery</span>
+                        <img height={45} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/manageitems">Manage Items</Nav.Link>
+                            <Nav.Link as={Link} to="/additems">Add Items</Nav.Link>
+                            <Nav.Link as={Link} to="/myitems">My Items</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            {
+                                user
+                                    ?
+                                    <Nav.Link as={Link} to="login" onClick={handleSignOut}>
+                                        Sign Out
+                                    </Nav.Link>
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
