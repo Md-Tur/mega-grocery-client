3b3f1aea676987ee1a87d345a7dca34dad8cef49
Md-Tur <turibnmostafiz@gmail.com>
2022-05-07 22:48:11 +0600

    spinner added for loading

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const Loading = () => {
     return (
-        <div>
-            <h2>Loading</h2>
+        <div style={{ height: '400px' }} className='w-100 d-flex justify-content-center align-items-center'>
+            <Spinner animation="grow" variant="success" />
         </div>
     );
 };
